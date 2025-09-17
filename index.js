4d33f0ffa124c4d6750872c212cfb70fc4fa2e00
unknown <pravatsutradhar>
2025-09-17 17:21:14 +0600

    .env add and basic route

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 3000;
 
+
+// Basic Route
+app.get('/', (req, res)=>{
+    res.send({
+        success: true,
+        data : "App is running on 5000"
+    })
+})
+
+
+// Server Start
 app.listen(port, ()=>{
-    console.log(`Server Running on http://localhost/${port}`)
+    console.log(`Server Running on http://localhost:${port}`)
 })
